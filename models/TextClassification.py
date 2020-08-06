2d9629022af0e1401e84187cdb38dd94e6996ce7
DocVir <viricel.clement@gmail.com>
2020-08-06 11:32:21 +0200

    add CNNTextclassif and huggingface connection

--- models/TextClassification.py
+++ models/TextClassification.py
@@ -34,10 +34,13 @@ class EncoderTextClassification(tf.keras.Model):
                  d_model,
                  num_heads,
                  dropout,
-                 get_attention=False,
                  final_units=1,
-                 name="encoder_text_classification"):
+                 activation=None,
+                 name="encoder_text_classification",
+                 pooling="average",
+                 kernel_regularizer=None):
         super(EncoderTextClassification, self).__init__(name=name)
+        self.pooling = pooling
         self.lambda_1 = tf.keras.layers.Lambda(create_padding_mask,
                                                output_shape=(1, 1, None),
                                                name='enc_padding_mask')
@@ -47,17 +50,66 @@ class EncoderTextClassification(tf.keras.Model):
                                d_model=d_model,
                                num_heads=num_heads,
                                dropout=dropout,
-                               name="encoder")
-        self.avg_pool = tf.keras.layers.GlobalAveragePooling1D()
-        self.dense = tf.keras.layers.Dense(units=final_units, name='outputs')
+                               name="encoder",
+                               kernel_regularizer=kernel_regularizer)
+        self.dense = tf.keras.layers.Dense(units=final_units,
+                                           activation=activation,
+                                           name='outputs')
 
     def call(self, inputs, get_attention=False):
         enc_padding_mask = self.lambda_1(inputs)
         enc_outputs, attention_weights_layers = self.encoder(inputs, enc_padding_mask)
-        enc_pool = self.avg_pool(enc_outputs)
+        if self.pooling == 'average':
+            enc_pool = tf.keras.layers.GlobalAveragePooling1D()(enc_outputs)
+        elif self.pooling == 'max':
+            enc_pool = tf.keras.layers.GlobalMaxPooling1D()(enc_outputs)
+        elif self.pooling == 'global_max':
+            outputs_max = tf.keras.layers.GlobalMaxPooling1D()(enc_outputs)
+            outputs_avg = tf.keras.layers.GlobalAveragePooling1D()(enc_outputs)
+            enc_pool = tf.keras.Concatenate()([outputs_max, outputs_avg])
+        elif self.pooling == "first":
+            enc_pool = tf.squeeze(enc_outputs[:, 0:1, :])
         outputs = self.dense(enc_pool)
         if get_attention:
             return outputs, attention_weights_layers
+        return outputs
+
+
+class HGEncoderTextClassification(tf.keras.Model):
+    def __init__(self,
+                 encoder,
+                 output_attentions=False,
+                 num_classes=1,
+                 activation=None,
+                 name="encoder_text_classification",
+                 pooling="average"):
+        super(HGEncoderTextClassification, self).__init__(name=name)
+        self.output_attentions = output_attentions
+        self.pooling = pooling
+        self.encoder = encoder
+        self.dense = tf.keras.layers.Dense(units=num_classes,
+                                           activation=activation,
+                                           name='final_outputs')
+
+    def call(self, inputs):
+        attention_mask = tf.keras.layers.Masking(mask_value=0)(inputs)
+        if self.output_attentions:
+            enc_outputs, pooler_output, attentions = self.encoder(inputs, attention_mask=attention_mask)
+        else:
+            enc_outputs, pooler_output = self.encoder(inputs, attention_mask=attention_mask)
+        if self.pooling == 'average':
+            enc_pool = tf.keras.layers.GlobalAveragePooling1D()(enc_outputs)
+        elif self.pooling == 'max':
+            enc_pool = tf.keras.layers.GlobalMaxPooling1D()(enc_outputs)
+        elif self.pooling == 'global_max':
+            outputs_max = tf.keras.layers.GlobalMaxPooling1D()(enc_outputs)
+            outputs_avg = tf.keras.layers.GlobalAveragePooling1D()(enc_outputs)
+            enc_pool = tf.keras.Concatenate()([outputs_max, outputs_avg])
+        elif self.pooling == "first":
+            enc_pool = pooler_output
+        outputs = self.dense(enc_pool)
+        if self.output_attentions:
+            return outputs, attentions
         return outputs
 
 
@@ -102,4 +154,85 @@ class TransformerTextClassification(tf.keras.Model):
         outputs = self.dense(outputs)
         if get_attention:
             return outputs, encoder_attention_weights_layers, decoder_attention_weights_layers
+        return outputs
+
+
+class CNNTextClassification(tf.keras.Model):
+    def __init__(self,
+                 kernel_sizes,
+                 filters,
+                 num_classes=1,
+                 dropout_rate=0,
+                 pooling='average',
+                 activation=None,
+                 name="transformer_text_classification",
+                 pre_embedded=False,
+                 vocab_size=0,
+                 units=128):
+        super(CNNTextClassification, self).__init__(name=name)
+        self.kernel_sizes = kernel_sizes
+        self.filters = filters
+        self.num_classes = num_classes
+        self.dropout_rate = dropout_rate
+        self.pooling = pooling
+        self.activation = activation
+        self.pre_embedded = pre_embedded
+        self.vocab_size = vocab_size
+        self.units = units
+        self.pooling = pooling
+        if not pre_embedded:
+            self.embedding = tf.keras.layers.Embedding(input_dim=vocab_size,
+                                                       output_dim=units,
+                                                       embeddings_initializer='random_normal',
+                                                       name='embedding')
+        else:
+            self.embedding = None
+        self.convs = []
+        for kernel_size in kernel_sizes:
+            self.convs.append(tf.keras.layers.Conv1D(filters,
+                                                     kernel_size=kernel_size,
+                                                     activation='relu',
+                                                     padding='same',
+                                                     name="conv1d_{}".format(kernel_size)))
+        if pooling == "average":
+            self.pool = tf.keras.layers.GlobalAveragePooling1D()
+        elif pooling == "max":
+            self.pool = tf.keras.layers.GlobalMaxPool1D()
+        else:
+            raise AttributeError("Pooling type not defined")
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
+        self.concat = tf.keras.layers.Concatenate()
+        self.flat = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense(units=units//2, activation='relu', name="outputs")
+        self.final_dense = tf.keras.layers.Dense(units=num_classes, activation=activation, name="outputs")
+
+    def get_config(self):
+        return {"kernel_sizes": self.kernel_sizes,
+                "filters": self.filters,
+                "num_classes": self.num_classes,
+                "dropout": self.dropout_rate,
+                "pooling": self.pooling,
+                "activation": self.activation,
+                "name": self.name,
+                "pre_embedded": self.pre_embedded,
+                "vocab_size": self.vocab_size,
+                "units": self.units}
+
+    def call(self, inputs, get_filters=False):
+        if self.embedding:
+            inputs = self.embedding(inputs)  # batch_size, words, emb
+        conv_outputs = []
+        argmax_filters = []
+        for conv in self.convs:
+            conv_output = conv(inputs)  # batch_size, words, filters
+            argmax_filters.append(tf.argmax(conv_output, axis=1))
+            conv_output = self.pool(conv_output)  # batch_size, filters
+            conv_output = self.dropout(conv_output)  # batch_size, words, filters
+            conv_outputs.append(conv_output)  # len(kerner_sizes), batch_size, words, filters
+        concat_convs = self.concat(conv_outputs)  # batch_size, words, len(kerner_sizes) * filters
+        flatten_conv = self.flat(concat_convs)
+        outputs = self.dense(flatten_conv)
+        outputs = self.final_dense(outputs)
+        if get_filters:
+            return outputs, conv_outputs, argmax_filters
         return outputs
